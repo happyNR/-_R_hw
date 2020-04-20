b175c5f0554d61cdba31c4cf1ecac11398c82a62
happyNR <63939244+happyNR@users.noreply.github.com>
2020-04-20 11:45:58 +0900

    Update mytranspose.R

--- mytranspose.R
+++ mytranspose.R
@@ -1,6 +1,6 @@
 mytranspose <- function(x) {
   if(is.vector(x) == TRUE){
-    x = as.matrix(t(x))
+    x = as.matrix(x)
     y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
     for(i in 1:nrow(x)) {
       for(j in 1:ncol(x)) {
