0523e3c16974854222008c5f40a1e6a1933fdbd9
happyNR <63939244+happyNR@users.noreply.github.com>
2020-04-20 09:50:53 +0900

    Add files via upload

---
+++ mytranspose.R
@@ -0,0 +1,34 @@
+mytranspose <- function(x) {
+  if(is.vector(x) == TRUE){
+    x = as.matrix(t(x))
+    y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
+    for(i in 1:nrow(x)) {
+      for(j in 1:ncol(x)) {
+        y[j,i] <- x[i,j]
+      }
+    }
+    return(as.vector(y))
+  }else if(is.data.frame(x) == TRUE){
+    x = as.matrix(x)
+    y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
+    for(i in 1:nrow(x)) {
+      for(j in 1:ncol(x)) {
+        y[j,i] <- x[i,j]
+      }
+    }
+    return(as.data.frame(y))
+  }else if(is.null(x) == TRUE){
+    return("null")
+  }
+  else if(is.logical(x) == TRUE){
+    return("logical")
+  }else{
+    y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
+    for(i in 1:nrow(x)) {
+      for(j in 1:ncol(x)) {
+        y[j,i] <- x[i,j]
+      }
+    }
+    return(y)
+  }
+}
